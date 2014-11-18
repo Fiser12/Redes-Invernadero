ecbc12a5b25a63102bec27f3db7a7bddf2acca02
Fiser12 <rubengh30@gmail.com>
2014-11-18 09:17:01 +0100

    Base de datos con sentencias de borrado y reinicio creada, falta cargar los valores base

--- src/servidor/serverModel/InteraccionDB.java
+++ src/servidor/serverModel/InteraccionDB.java
@@ -11,8 +11,26 @@ public class InteraccionDB {
 	{
 		gestor.enviarComando("CREATE TABLE IF NOT EXISTS Usuario (Nombre VARCHAR(50) NOT NULL ,Pass VARCHAR(50) NOT NULL, PRIMARY KEY(Nombre));");
 		gestor.enviarComando("CREATE TABLE IF NOT EXISTS Placa (Id INT(10) NOT NULL AUTOINCREMENT,Estado VARCHAR(250) NOT NULL, Foto VARCHAR(250), PRIMARY KEY (Id));");
+		gestor.enviarComando("CREATE TABLE IF NOT EXISTS Variable (Nombre VARCHAR(250) NOT NULL,Funcion_Principal VARCHAR(500) NOT NULL, PRIMARY KEY(Nombre));");
 		gestor.enviarComando("CREATE TABLE IF NOT EXISTS Usuario_Placa(Nombre VARCHAR(50) NOT NULL, Id_Placa INT(10) NOT NULL, CONSTRAINT Nombre FOREIGN KEY (Nombre) REFERENCES Usuario (Nombre) ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT Id_Placa FOREIGN KEY (Id_Placa) REFERENCES Placa(Id) ON DELETE CASCADE ON UPDATE CASCADE");
 		gestor.enviarComando("CREATE TABLE IF NOT EXISTS Sensor (Id INT(10) NOT NULL AUTOINCREMENT, Nombre_Variable VARCHAR(250) NOT NULL, Estado_la_variable VARCHAR(250) NOT NULL,Ultima_Accion VARCHAR(500) NOT NULL, id_Placa INT(10), PRIMARY KEY(Id, id_Placa), CONSTRAINT id_Placa FOREIGN KEY (id_Placa) REFERENCES Placa (Id) ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT Nombre_Variable FOREIGN KEY (Nombre_Variable) REFERENCES Variable(Nombre) ON DELETE CASCADE ON UPDATE CASCADE);");
-		gestor.enviarComando("CREATE TABLE IF NOT EXISTS Variable (Nombre VARCHAR(250) NOT NULL,Funcion_Principal VARCHAR(500) NOT NULL, PRIMARY KEY(Nombre));");
+	}
+	private static void borrarBaseDeDatos()
+	{
+		gestor.enviarComando("DROP TABLE IF EXISTS Sensor");
+		gestor.enviarComando("DROP TABLE IF EXISTS Usuario_Placa");
+		gestor.enviarComando("DROP TABLE IF EXISTS Variable");
+		gestor.enviarComando("DROP TABLE IF EXISTS Placa");
+		gestor.enviarComando("DROP TABLE IF EXISTS Usuario");
+	}
+	public static void reiniciarBase()
+	{
+		borrarBaseDeDatos();
+		crearBaseDeDatos();
+		cargarDatosIniciales();
+	}
+	public static void cargarDatosIniciales()
+	{
+		
 	}
 }
