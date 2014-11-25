18915fe601dcf2754efd22b75dbd49dd927c2dc3
Fiser12 <Fiser@192.168.0.17>
2014-11-25 03:17:52 +0100

    El método listar visualiza en la lista ya todas las cosas correctamente

--- src/cliente/view/VentanaTabla.java
+++ src/cliente/view/VentanaTabla.java
@@ -7,14 +7,13 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.JScrollPane;
 
 import java.awt.GridLayout;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
@@ -22,12 +21,17 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.ListSelectionModel;
 
 import servidor.serverModel.ModelClass.Sensor;
-import servidor.serverModel.ModelClass.Placa;
-import servidor.serverModel.ModelClass.Usuario;
+import util.Util;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-public class VentanaTabla extends JFrame implements FocusListener{
+import java.io.IOException;
+public class VentanaTabla extends JFrame{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField textIDPlaca;
 	private JTextField textIdSensor;
@@ -35,13 +39,13 @@ public class VentanaTabla extends JFrame implements FocusListener{
 	private JButton btnActivar;
 	private JButton btnDesactivar;
 	private JTextField textVariable;
-	private JComboBox comboBoxBusqueda;
+	private JComboBox<String> comboBoxBusqueda;
 	private JButton btnBuscar;
 	private JButton btnListar;
-	private jTable modeloTabla;
+	private ModeloSensor modeloTabla;
 	private JTable tabla;
-	private ArrayList<Placa> TodasPlacas=new ArrayList<Placa>();
-	private ArrayList<Placa> PlacasDeBusqueda=new ArrayList();
+    private String respuesta;
+
 	/**
 	 * Launch the application.
 	 */
@@ -65,7 +69,6 @@ public class VentanaTabla extends JFrame implements FocusListener{
 		/*
 		 * Rellenar el array list De TodasPlacas
 		 */
-	    PlacasDeBusqueda=TodasPlacas;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 550);
 		contentPane = new JPanel();
@@ -76,23 +79,11 @@ public class VentanaTabla extends JFrame implements FocusListener{
 		JPanel BotonesArriba = new JPanel();
 		contentPane.add(BotonesArriba, BorderLayout.NORTH);
 		BotonesArriba.setLayout(new GridLayout(0, 6, 0, 0));
-		modeloTabla=new jTable();
-		modeloTabla.setColumnIdentifiers(new String[]{"SENSOR","PLACA","VARIABLE","ESTADO","FUNCION","ULTIMA ACCION"});
-		
-		for(int i=0;i<TodasPlacas.size();i++){
-			Placa p=TodasPlacas.get(i);
-			for(int j=0;j<p.getSensores().size();j++)
-		/*
-		 * Rellenar tabla
-		 */
-			modeloTabla.addRow(new String[] {""+p.getId()});
-		}
-		
-	    btnImagen = new JButton("Imajen Placa");
+			
+	    btnImagen = new JButton("Imagen Placa");
 		BotonesArriba.add(btnImagen);
 		
 		textIDPlaca = new JTextField();
-		textIDPlaca.setText("IDPlaca");
 		BotonesArriba.add(textIDPlaca);
 		textIDPlaca.setColumns(10);
 		
@@ -102,204 +93,186 @@ public class VentanaTabla extends JFrame implements FocusListener{
 		textIdSensor.setColumns(10);
 		
 		btnActivar = new JButton("Activar");
+		BotonesArriba.add(btnActivar);
 		btnActivar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-			int Filaseleccionada=tabla.getSelectedRow();
-			Placa p=PlacasDeBusqueda.get(Filaseleccionada);
-			/*
-			 * Enviar etring al servidor 
-			 */
+				activar();
 			}
 		});
-		BotonesArriba.add(btnActivar);
 		
 		btnDesactivar = new JButton("Desactivar");
 		btnDesactivar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				int Filaseleccionada=tabla.getSelectedRow();
-				Placa p=PlacasDeBusqueda.get(Filaseleccionada);
-				/*
-				 * Enviar string al servidor 
-				 */
-				
+				desactivar();
 			}
 		});
 		BotonesArriba.add(btnDesactivar);
 		
-		JButton btnActuar = new JButton("Actuar\r\n");
+		JButton btnActuar = new JButton("Actuar");
 		BotonesArriba.add(btnActuar);
 		
 		JPanel BotonesAbajo = new JPanel();
 		contentPane.add(BotonesAbajo, BorderLayout.SOUTH);
 		BotonesAbajo.setLayout(new GridLayout(2, 1, 0, 0));
-		
+
 		JPanel Busqueda = new JPanel();
 		BotonesAbajo.add(Busqueda);
 		Busqueda.setLayout(new GridLayout(0, 3, 0, 0));
-		
+
 		textVariable = new JTextField();
 		Busqueda.add(textVariable);
 		textVariable.setColumns(10);
-		
-		comboBoxBusqueda = new JComboBox();
+
+		comboBoxBusqueda = new JComboBox<String>();
 		comboBoxBusqueda.setModel(new DefaultComboBoxModel(new String[] {"Elegir Opcion De Busqueda", "Todas", "IDPlaca", "Variable", "IDSensor", "En OFF", "EN ON", ""}));
 		comboBoxBusqueda.setToolTipText("Elegir opcion\r\n");
 		Busqueda.add(comboBoxBusqueda);
-		
-		 btnBuscar = new JButton("Buscar");
-		 btnBuscar.addActionListener(new ActionListener() {
-		 	public void actionPerformed(ActionEvent arg0) {
-		 	buscar();
-		 	}
-		 });
+
+		btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				buscar();
+			}
+		});
+
 		Busqueda.add(btnBuscar);
-		
+
 		JPanel Listar = new JPanel();
 		BotonesAbajo.add(Listar);
 		Listar.setLayout(new GridLayout(0, 2, 0, 0));
-		
-		 btnListar = new JButton("Listar");
+
+		btnListar = new JButton("Listar");
+		btnListar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				listar();
+			}
+		});
 		Listar.add(btnListar);
-		
+
 		JButton btnSalir = new JButton("Salir");
 		btnSalir.addActionListener(new ActionListener() {
+			@Override
 			public void actionPerformed(ActionEvent arg0) {
-			dispose();}
+				salir();
+			}
 		});
 		Listar.add(btnSalir);
-		JScrollPane sPTabla = new JScrollPane();
+		modeloTabla=new ModeloSensor();
 		tabla=new JTable(modeloTabla);
-		tabla.setModel(modeloTabla);
-		tabla.addFocusListener(this);
-		tabla.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		sPTabla.add(tabla);
-		contentPane.add(sPTabla, BorderLayout.CENTER);
+	    tabla.setCellSelectionEnabled(true);
+	    tabla.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		JScrollPane scroll = new JScrollPane(tabla);
+		contentPane.add(scroll, BorderLayout.CENTER);
+	}
+	public  void buscar(){
+
+	}
+	public void salir(){
+		try {
+			Util.claseSocketCliente.Escribir("SALIR\n");
+			respuesta = Util.claseSocketCliente.Leer();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null,respuesta,"Error",JOptionPane.ERROR_MESSAGE); 
+		}
+		dispose();
+	}
+	public void activar(){
+        int rowIndex = tabla.getSelectedRow();
+        Sensor seleccionado = new Sensor(Integer.parseInt((String) tabla.getValueAt(rowIndex, 0)), (String)tabla.getValueAt(rowIndex, 2), (String)tabla.getValueAt(rowIndex, 4), (String)tabla.getValueAt(rowIndex, 3), (String)tabla.getValueAt(rowIndex, 5), Integer.parseInt((String) tabla.getValueAt(rowIndex, 1)));
+        
+	}
+	public void desactivar(){
+		
+	}
+	public void listar(){
+		try {
+			Util.claseSocketCliente.Escribir("LISTADO\n");
+			respuesta = Util.claseSocketCliente.Leer().replace("/n", "\n");
+			LinkedList<Sensor> sensores = convertirASensor(sacarRespuesta(respuesta));
+			for(Sensor temp: sensores){
+				modeloTabla.addRow(temp);
+			}
+			modeloTabla.fireTableDataChanged();
+			tabla.setModel(modeloTabla);
+			repaint();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null,respuesta,"Error",JOptionPane.ERROR_MESSAGE); 
+		}
+
+	}
+	private LinkedList<String> sacarRespuesta(String respuesta)
+	{
+		LinkedList<String>sacadas = new LinkedList<String>();
+		modeloTabla = new ModeloSensor();
+		String temporal = respuesta.substring(0, respuesta.indexOf("\n"));
+		respuesta = respuesta.substring(respuesta.indexOf("\n")+1);
+
+		while(respuesta.contains("\n"))
+		{
+			sacadas.add(temporal);
+			temporal = respuesta.substring(0, respuesta.indexOf("\n"));
+			respuesta = respuesta.substring(respuesta.indexOf("\n")+1);
+		}
+		return sacadas;
+	}
+	private LinkedList<Sensor> convertirASensor(LinkedList<String> temp)
+	{
+		LinkedList<Sensor> devolver = new LinkedList<Sensor>();
+		
+		for(String tratar: temp)
+		{
+			System.out.println(tratar);
+			Sensor nuevo = new Sensor();
+			nuevo.setId_Placa(Character.getNumericValue(tratar.charAt(11)));
+			nuevo.setId_sensor(Character.getNumericValue(tratar.charAt(4)));
+			nuevo.setUltimaAccion(tratar.substring(tratar.lastIndexOf(";")+2));
+			nuevo.setVariable(tratar.substring(14, tratar.indexOf(";", 14)));
+			nuevo.setFuncionPrincipal(tratar.substring(tratar.indexOf(";", tratar.indexOf(";", 14))+2,tratar.indexOf(";", tratar.indexOf(";", 14)+2)));
+			nuevo.setEstadoVariable(tratar.substring(tratar.lastIndexOf(";", tratar.lastIndexOf(";")-1)+2,tratar.lastIndexOf(";")));
+			devolver.add(nuevo);
+		}
+		return devolver;
+	}
+	public void obtenerFoto()
+	{
+		
+	}
+	public void accion()
+	{
+		
 	}
 	/**
 	 * Clase creada para hacer no editable una tabla
 	 *
 	 */
-	public class jTable extends DefaultTableModel
+	public class ModeloSensor extends DefaultTableModel
 	{
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 
 		/**
 		 * Metodo utilizado para no editar la tabla
 		 */
+		public ModeloSensor(){
+			setColumnIdentifiers(new String[]{"Sensor","Placa","Variable","Estado","Funcion","Ultima Accion"});
+		}
 		public boolean isCellEditable (int row, int column)
 		{
-			// Aqu� devolvemos true o false seg�n queramos que una celda
-			// identificada por fila,columna (row,column), sea o no editable
-			if (column >=0)
-				return false;
-			return true;
+			return false;
 		}
-	}
-	@Override
-	public void focusGained(FocusEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void focusLost(FocusEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	public  void buscar(){
-	 modeloTabla=new jTable();
-	 comboBoxBusqueda.setModel(new DefaultComboBoxModel(new String[] {"Elegir Opcion De Busqueda", "Todas", "IDPlaca", "Variable", "IDPLaca", "En OFF", "EN ON", ""}));
-	 if(comboBoxBusqueda.getSelectedIndex()==1){
-			for(int i=0;i<TodasPlacas.size();i++){
-				Placa p=TodasPlacas.get(i);
-				for(int j=0;j<p.getSensores().size();j++){
-					PlacasDeBusqueda.removeAll(null);
-					PlacasDeBusqueda.add(p);
-			 /*
-			 * Rellenar tabla
-			 */
-				modeloTabla.addRow(new String[] {""+p.getId()});}
-			}}
-	 if(comboBoxBusqueda.getSelectedIndex()==2)
-	 { 
-		 for(int i=0;i<TodasPlacas .size();i++){
-			 Placa p=TodasPlacas.get(i);
-			 for(int j=0;j<p.getSensores().size();j++){
-				 if(textVariable.getText().equals(""+p.getId())){
-					 /*
-					  * Rellenar tabla 
-					  */
-						PlacasDeBusqueda.removeAll(null);
-						PlacasDeBusqueda.add(p);
-					 modeloTabla.addRow(new String[] {""+p.getId()});}
-			 }
-		 }
-
-	 }
-	 
-	 if(comboBoxBusqueda.getSelectedIndex()==3)
-	 { 
-		 for(int i=0;i<TodasPlacas.size();i++){
-			 Placa p=TodasPlacas.get(i);
-			 for(int j=0;j<p.getSensores().size();j++){
-				 if(textVariable.getText().equals(""+p.getId())){
-					 /*
-					  * Rellenar tabla con la variable del sensor pero como no se como sacarla
-					  */
-						PlacasDeBusqueda.removeAll(null);
-						PlacasDeBusqueda.add(p);
-					 modeloTabla.addRow(new String[] {""+p.getId()});}
-			 }
-		 }
-
-	 }
-	 
-	 if(comboBoxBusqueda.getSelectedIndex()==4)
-	 { 
-		 for(int i=0;i<TodasPlacas.size();i++){
-			 Placa p=TodasPlacas.get(i);
-			 for(int j=0;j<p.getSensores().size();j++){
-				 if(textVariable.getText().equals(""+p.getId())){
-					 /*
-					  * Rellenar tabla con la id del sensor 
-					  */
-						PlacasDeBusqueda.removeAll(null);
-						PlacasDeBusqueda.add(p);
-					 modeloTabla.addRow(new String[] {""+p.getId()});}
-			 }
-		 }
-
-	 }
-	 if(comboBoxBusqueda.getSelectedIndex()==5)
-	 { 
-		 for(int i=0;i<TodasPlacas.size();i++){
-			 Placa p=TodasPlacas.get(i);
-			 for(int j=0;j<p.getSensores().size();j++){
-				 if("ON".equals(""+p.getId())){
-					 /*
-					  * Rellenar tabla con el estado del sensor
-					  */
-						PlacasDeBusqueda.removeAll(null);
-						PlacasDeBusqueda.add(p);
-					 modeloTabla.addRow(new String[] {""+p.getId()});}
-			 }
-		 }
-
-	 }
-	 if(comboBoxBusqueda.getSelectedIndex()==6)
-	 { 
-		 for(int i=0;i<TodasPlacas.size();i++){
-			 Placa p=TodasPlacas.get(i);
-			 for(int j=0;j<p.getSensores().size();j++){
-				 if("OFF".equals(""+p.getId())){
-					 /*
-					  * Rellenar tabla con el estado del sensor
-					  */
-						PlacasDeBusqueda.removeAll(null);
-						PlacasDeBusqueda.add(p);
-					 modeloTabla.addRow(new String[] {""+p.getId()});}
-			 }
-		 }
-
-	 }
+		public void addRow(Sensor añadir){
+			String idSensor = ""+añadir.getId_sensor();
+			String idPlaca = ""+añadir.getId_Placa();
+			String variable = añadir.getVariable();
+			String estado = añadir.getEstadoVariable();
+			String funcion = añadir.getFuncionPrincipal();
+			String accion = añadir.getUltimaAccion();
+			Object [] add = {idSensor, idPlaca, variable, estado, funcion, accion};
+			this.addRow(add);
+		}
 	}
 }
